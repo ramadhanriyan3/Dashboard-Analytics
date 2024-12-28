8cccb89ad7adf233768bc9bd8e1a22644be03277
Riyan Ramadhan <ramadhanriyan3@gmail.com>
2024-12-28 20:59:24 +0700

    fix: seachParamsError

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,16 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 const PrivateLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full h-full relative">
       <Sidebar />
       <Navbar />
-      <div className="ml-[90px] lg:ml-[280px] mr-4 pt-20 ">{children}</div>
+      <Suspense>
+        <div className="ml-[90px] lg:ml-[280px] mr-4 pt-20 ">{children}</div>
+      </Suspense>
     </div>
   );
 };
